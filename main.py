f9f911b850009172710b608a5cd511ddee821aab
Kostia2004 <k.a.kudriavsev@mail.ru>
2022-06-11 00:47:36 +0300

    audio downloading

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+import youtube_dl
+import sys
+
+
+def main():
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([input('URL: ')])
+
+if __name__=='__main__':
+    main()
